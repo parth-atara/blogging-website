a821c31a40df76540ace8ab94cc46636aaeca883
Parth Atara <parthatara111@gmail.com>
2022-06-21 15:14:44 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -75,5 +75,10 @@ app.get("/posts/:postName", function(req, res){
 });
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("Server has started successfully.");
 });
